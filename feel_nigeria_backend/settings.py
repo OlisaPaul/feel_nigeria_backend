2fc95c56431d3a6340d424fbd824e365ebb80b6e
OlisaPaul <odirahchukwumma28@gmail.com>
2025-04-26 08:16:31 +0100

    Add django-cors-headers to Pipfile and configure CORS settings in Django

--- feel_nigeria_backend/settings.py
+++ feel_nigeria_backend/settings.py
@@ -46,11 +46,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'djoser',
+    'corsheaders',
     'core',
     'store',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -160,3 +162,7 @@ SIMPLE_JWT = {
     'TOKEN_TYPE_CLAIM': 'token_type',
     'JTI_CLAIM': 'jti',
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
